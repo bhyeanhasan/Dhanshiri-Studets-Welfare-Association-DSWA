75a04d9c64e49082d301674a99e1a54e4d8d2584
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2022-01-09 23:53:19 +0600

    new

--- APP_Member/urls.py
+++ APP_Member/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('student', views.student_register, name="student_register"),
-    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('profile', views.profile, name="profile"),
+    path('login',views.login_user, name='login'),
+    path('logout',views.logout_user, name='logout'),
 ]
--- APP_Member/views.py
+++ APP_Member/views.py
@@ -1,18 +1,40 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import Student_form
+from .models import Student
 
 
 def index(request):
     return render(request, 'index.html')
 
 
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+    return render(request, "registration/login.html", {})
 
-def student_register(request):
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
+
+
+@login_required(login_url='login')
+def profile(request):
+
+    profile_check = Student.objects.get(user=request.user)
+
     if request.method == 'POST':
         form = Student_form(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('index')
     else:
         form = Student_form()
 
