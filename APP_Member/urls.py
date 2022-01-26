6d483026802d9e86124c0f77086fed8579cac039
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2022-01-26 12:34:07 +0600

    new

--- APP_Member/urls.py
+++ APP_Member/urls.py
@@ -6,8 +6,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name="index"),
     path('profile', views.profile, name="profile"),
-    path('register', views.register, name='register'),
     path('contact',views.contact, name='contact'),
     path('login',views.login_user, name='login'),
     path('logout',views.logout_user, name='logout'),
+    path('user_signup', views.user_signup, name='user_signup'),
+    path('register', views.register, name='register'),
+    path('update', views.update, name='update'),
+
 ]
--- APP_Member/views.py
+++ APP_Member/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .form import Student_form
@@ -22,7 +23,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             return redirect("index")
-    return render(request, "registration/login.html", {})
+    return render(request, "member/login.html", {})
 
 
 def logout_user(request):
@@ -34,7 +35,7 @@ def logout_user(request):
 def profile(request):
     try:
         user_profile = Student.objects.get(user=request.user)
-        return render(request, 'profile.html', {'user_profile': user_profile})
+        return render(request, 'member/profile.html', {'user_profile': user_profile})
     except:
         return redirect('register')
 
@@ -53,10 +54,50 @@ def register(request):
             phone = form.cleaned_data['phone']
             blood = form.cleaned_data['blood']
 
-            Student.objects.create(user=user,name=name,faculty=faculty,session=session,gender=gender,upazila=upazila,phone=phone,blood=blood)
+            Student.objects.create(user=user, name=name, faculty=faculty, session=session, gender=gender,
+                                   upazila=upazila, phone=phone, blood=blood)
 
             return redirect('profile')
     else:
         form = Student_form()
 
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'member/register.html', {'form': form})
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['password']
+        pass2 = request.POST['password2']
+        print(username + email + pass1 + pass2)
+        if pass1 == pass2:
+            User.objects.create_user(username, email, pass1)
+            return redirect('register')
+
+        return render(request, 'member/user signup.html')
+    else:
+        return render(request, 'member/user signup.html')
+
+
+def update(request):
+    student = Student.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        faculty = request.POST['faculty']
+        session = request.POST['session']
+        gender = request.POST['gender']
+        upazila = request.POST['upazila']
+        phone = request.POST['phone']
+        blood = request.POST['blood']
+        student.name = name
+        student.faculty = faculty
+        student.session = session
+        student.gender = gender
+        student.upazila = upazila
+        student.phone = phone
+        student.blood = blood
+        student.save()
+        return redirect('profile')
+    return render(request,'member/update profile.html')
