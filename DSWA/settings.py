fe1e65c927ba35e8e2063d085e6f58625c21bae7
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-09-02 23:44:00 +0600

    new

--- DSWA/settings.py
+++ DSWA/settings.py
@@ -80,10 +80,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
+                    os.path.join(STATIC_ROOT, 'image/')
+                  )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
