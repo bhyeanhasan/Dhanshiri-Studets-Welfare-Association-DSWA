e0aedde841fcdca81a18c53494e29f50ea6082eb
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-09-05 02:08:20 +0600

    fg

--- APP_Member/urls.py
+++ APP_Member/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('registration/', include('django.contrib.auth.urls')),
     path('', views.index, name="index"),
@@ -25,5 +26,4 @@ urlpatterns = [
     path('notice', views.notice, name='notice'),
     path('view/<id>/', views.view, name='view'),
     path('delete_notice/<id>/', views.delete_notice, name='delete_notice'),
-
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- DSWA/urls.py
+++ DSWA/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('APP_Member.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
